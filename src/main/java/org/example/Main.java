87892462e2aeed7c8e9933c317f5e29c989ca852
GermanRandhawaa <germanpreet3533@gmail.com>
2022-09-09 10:39:21 -0700

    this is a commit for lab1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("This is unique message for 2522.");
+        System.out.println("LAB1 is done ");
 
     }
 }
